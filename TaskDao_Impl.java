c29546e175057d92019f45ffee5ba82bb266e473
anilsangar121996-maker <anil.sangar121996@marwadiuniversity.ac.in>
2026-01-28 14:20:05 +0530

    Add files via upload

---
+++ TaskDao_Impl.java
@@ -0,0 +1,182 @@
+package com.notification_reminder.database;
+
+import android.database.Cursor;
+import androidx.annotation.NonNull;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Class;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.annotation.processing.Generated;
+
+@Generated("androidx.room.RoomProcessor")
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class TaskDao_Impl implements TaskDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<Task> __insertionAdapterOfTask;
+
+  private final EntityDeletionOrUpdateAdapter<Task> __deletionAdapterOfTask;
+
+  private final EntityDeletionOrUpdateAdapter<Task> __updateAdapterOfTask;
+
+  public TaskDao_Impl(@NonNull final RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfTask = new EntityInsertionAdapter<Task>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "INSERT OR ABORT INTO `tasks` (`id`,`title`,`description`,`timeMillis`,`isCompleted`) VALUES (nullif(?, 0),?,?,?,?)";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement, final Task entity) {
+        statement.bindLong(1, entity.id);
+        if (entity.title == null) {
+          statement.bindNull(2);
+        } else {
+          statement.bindString(2, entity.title);
+        }
+        if (entity.description == null) {
+          statement.bindNull(3);
+        } else {
+          statement.bindString(3, entity.description);
+        }
+        statement.bindLong(4, entity.timeMillis);
+        final int _tmp = entity.isCompleted ? 1 : 0;
+        statement.bindLong(5, _tmp);
+      }
+    };
+    this.__deletionAdapterOfTask = new EntityDeletionOrUpdateAdapter<Task>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "DELETE FROM `tasks` WHERE `id` = ?";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement, final Task entity) {
+        statement.bindLong(1, entity.id);
+      }
+    };
+    this.__updateAdapterOfTask = new EntityDeletionOrUpdateAdapter<Task>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "UPDATE OR ABORT `tasks` SET `id` = ?,`title` = ?,`description` = ?,`timeMillis` = ?,`isCompleted` = ? WHERE `id` = ?";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement, final Task entity) {
+        statement.bindLong(1, entity.id);
+        if (entity.title == null) {
+          statement.bindNull(2);
+        } else {
+          statement.bindString(2, entity.title);
+        }
+        if (entity.description == null) {
+          statement.bindNull(3);
+        } else {
+          statement.bindString(3, entity.description);
+        }
+        statement.bindLong(4, entity.timeMillis);
+        final int _tmp = entity.isCompleted ? 1 : 0;
+        statement.bindLong(5, _tmp);
+        statement.bindLong(6, entity.id);
+      }
+    };
+  }
+
+  @Override
+  public void insert(final Task task) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __insertionAdapterOfTask.insert(task);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void delete(final Task task) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __deletionAdapterOfTask.handle(task);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void update(final Task task) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __updateAdapterOfTask.handle(task);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public List<Task> getAllTasks() {
+    final String _sql = "SELECT * FROM tasks ORDER BY timeMillis ASC";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    __db.assertNotSuspendingTransaction();
+    final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+    try {
+      final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+      final int _cursorIndexOfTitle = CursorUtil.getColumnIndexOrThrow(_cursor, "title");
+      final int _cursorIndexOfDescription = CursorUtil.getColumnIndexOrThrow(_cursor, "description");
+      final int _cursorIndexOfTimeMillis = CursorUtil.getColumnIndexOrThrow(_cursor, "timeMillis");
+      final int _cursorIndexOfIsCompleted = CursorUtil.getColumnIndexOrThrow(_cursor, "isCompleted");
+      final List<Task> _result = new ArrayList<Task>(_cursor.getCount());
+      while (_cursor.moveToNext()) {
+        final Task _item;
+        final String _tmpTitle;
+        if (_cursor.isNull(_cursorIndexOfTitle)) {
+          _tmpTitle = null;
+        } else {
+          _tmpTitle = _cursor.getString(_cursorIndexOfTitle);
+        }
+        final String _tmpDescription;
+        if (_cursor.isNull(_cursorIndexOfDescription)) {
+          _tmpDescription = null;
+        } else {
+          _tmpDescription = _cursor.getString(_cursorIndexOfDescription);
+        }
+        final long _tmpTimeMillis;
+        _tmpTimeMillis = _cursor.getLong(_cursorIndexOfTimeMillis);
+        _item = new Task(_tmpTitle,_tmpDescription,_tmpTimeMillis);
+        _item.id = _cursor.getInt(_cursorIndexOfId);
+        final int _tmp;
+        _tmp = _cursor.getInt(_cursorIndexOfIsCompleted);
+        _item.isCompleted = _tmp != 0;
+        _result.add(_item);
+      }
+      return _result;
+    } finally {
+      _cursor.close();
+      _statement.release();
+    }
+  }
+
+  @NonNull
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
